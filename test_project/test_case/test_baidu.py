d8c01157d50d84090ec1348e55eb50a8dbe2d832
rock <1640191389@qq.com>
2019-08-07 16:33:38 +0800

    生成HTML用例测试报告

--- test_project/test_case/test_baidu.py
+++ test_project/test_case/test_baidu.py
@@ -2,7 +2,7 @@ import unittest
 
 import time
 from selenium import webdriver
-
+from HtmlTestRunner import HTMLTestRunner
 
 class My_Test(unittest.TestCase):
     def setUp(self):
@@ -23,5 +23,15 @@ class My_Test(unittest.TestCase):
         self.driver.quit()
 if __name__ == '__main__':
 
-    unittest.main()
+   testnit = unittest.TestSuite()
+   testnit.addTest(My_Test('test_baidu'))
+   fp = open('./result.html','wb')
+   runner = HTMLTestRunner(stream=fp,
+                           report_title='百度搜索用例报告',
+                           descriptions='用例执行情况'
+   )
+   runner.run(testnit)
+   fp.close()
 
+#通过以上办法无法生成测试报告，最简单的办法看连接
+#https://blog.csdn.net/weixin_38981172/article/details/82389416
