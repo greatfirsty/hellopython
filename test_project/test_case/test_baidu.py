03373b64ddbad15c16bbfed08bae498285b59c32
rock <1640191389@qq.com>
2019-08-07 17:18:34 +0800

    多级文件生成HTML用例测试报告，用于测试集成

--- test_project/test_case/test_baidu.py
+++ test_project/test_case/test_baidu.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from HtmlTestRunner import HTMLTestRunner
 
 class My_Test(unittest.TestCase):
+    ''' 百度搜索测试'''
     def setUp(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
@@ -18,20 +19,11 @@ class My_Test(unittest.TestCase):
         driver.find_element_by_id('su').click()
         time.sleep(2)
         title = driver.title
-        self.assertEqual(title,'unittest_百度搜索')
+        self.assertEqual(title,'美女_百度搜索')
     def tearDown(self):
         self.driver.quit()
 if __name__ == '__main__':
 
-   testnit = unittest.TestSuite()
-   testnit.addTest(My_Test('test_baidu'))
-   fp = open('./result.html','wb')
-   runner = HTMLTestRunner(stream=fp,
-                           report_title='百度搜索用例报告',
-                           descriptions='用例执行情况'
-   )
-   runner.run(testnit)
-   fp.close()
-
+  unittest.main()
 #通过以上办法无法生成测试报告，最简单的办法看连接
 #https://blog.csdn.net/weixin_38981172/article/details/82389416
--- test_project/testrun.py
+++ test_project/testrun.py
@@ -1,6 +1,13 @@
 import unittest
+
+import time
+from HtmlTestRunner import HTMLTestRunner
 test_dir = '../test_project/test_case'
 discovery = unittest.defaultTestLoader.discover(test_dir,pattern='test*.py')
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
+    now = time.strftime('%Y_m_d %H_%M_%S')
+    filename =test_dir+ './'+now + 'result.html'
+    fp = open(filename,'w')
+    runner = HTMLTestRunner(stream=fp,report_title='测试报告',descriptions='用例执行情况')
     runner.run(discovery)
+    fp.close()
