76af4b33c3cd97257519c25cd1667d72994053cb
rock <1640191389@qq.com>
2019-08-07 08:36:28 +0800

    测试用例

--- right_click_action.py
+++ right_click_action.py
@@ -1,15 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver import ActionChains
+class Count:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a+self.b
+    def sub(self):
+        return self.a+self.b
 
-dr = webdriver.Chrome()
-dr.get('https://yunpan.360.cn/')
-account = dr.find_element_by_name("account")
-account.clear()
-account.send_keys('183xxxx')
 
-password = dr.find_element_by_name("password")
-password.clear()
-password.send_keys('hxxxxxxx')
-left = dr.find_element_by_xpath("//input[@class='quc-submit quc-button quc-button-primary quc-button-sign-in']")
-ActionChains(dr).context_click(left).perform()
-#这里是鼠标右击操作，单击直接可以click
--- shexiangtou.py
+++ shexiangtou.py
@@ -1,16 +1,50 @@
-import cv2
-cv2.namedWindow('Image')
+import unittest
 
-aa = cv2.VideoCapture(0)
-while (aa.isOpened):
-    ret, img = aa.read()
-    if ret == True:
-        cv2.imshow('Image',img)
-        k = cv2.waitKey(100)
-        if k == ord('a') or k ==ord('A'):
-            cv2.imwrite('test.jpg',img)
-            break
-aa.close()
-cv2.waitKey(0)
-cv2.destryAllWindow()
+from right_click_action import Count
+
+
+class Test(unittest.TestCase):
+    def setUp(self,):
+        print('test starting')
+
+    def test_case(self):
+        self.assertEqual(self.number,10,msg = 'you number is not 10!')
+    def test_add(self):
+        self.j =Count(2,3)
+        self.add = self.j.add()
+        self.assertEqual(self.add,5)
+    def test_add2(self):
+        self.j = Count(2.0,3.0)
+        self.add = self.j.add()
+        self.assertEqual(self.add,5.0)
+    def test_add3(self):
+        self.j = Count('hello','world')
+        self.add = self.j.add()
+        self.assertEqual(self.add,'hello world')
+    def test_sub(self):
+        self.j = Count(2.1,3)
+        self.sub = self.j.sub()
+        self.assertEqual(self.sub,-0.9)
+
+    def tearDown(self):
+        pass
+class Test_Sub(unittest.TestCase):
+    def setUp(self):
+        print('test start')
+
+    def test_sub(self):
+        self.j =Count(2.3,3.0)
+        self.sub = self.j.sub()
+        self.assertEqual(self.sub,-0.7)
+    def tearDown(self):
+        pass
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    suite.addTest(Test('test_add'))
+    suite.addTest(Test('test_add2'))
+    suite.addTest(Test('test_add3'))
+    suite.addTest(Test_Sub('test_sub'))
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
+
 
