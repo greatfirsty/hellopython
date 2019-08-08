93f717914cf8a35638b90e32dc41607679ed765f
rock <1640191389@qq.com>
2019-08-08 10:39:39 +0800

    添加向邮箱发送附件

--- Smtp1_mail.py
+++ Smtp1_mail.py
@@ -1,3 +1,4 @@
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 mail_content = '''
 <!DOCTYPE html>
@@ -11,17 +12,30 @@ mail_content = '''
 </body>
 </html>
 '''
-msg = MIMEText(mail_content,'html','utf-8')
-from_addr = '1640191389@qq.com'
-from_pwd = 'hbftrb'
+#msg = MIMEText(mail_content,'html','utf-8')
+#发送附件
+send_file = open(r"C:\\Users\hss\workgit\hellopython\Test Results - Twisted_Trial_for_test_baidu_My_Test_test_baidu.html",'rb').read()
+att = MIMEText(send_file,'base64','utf-8')
+att['content-Type'] = 'application/octet-stream'
+att['content-Disposition'] = 'attachment;filename="log.txt"'
+#这里主要是对附件的格式
+msgrot = MIMEMultipart('related')
+subject = '发送测试报告'
+msgrot['Subject'] = subject
+msgrot.attach(att)
 
+from_addr = '1640191389@qq.com'
+from_pwd = 'goinknvgbiz'#授权码
 to_addr = '18330016979@163.com'
 smtp_srv = 'smtp.qq.com'
+
 try:
     import smtplib
     srv = smtplib.SMTP_SSL(smtp_srv.encode(),465)
     srv.login(from_addr,from_pwd)
-    srv.sendmail(from_addr,[to_addr],msg.as_string())
+    srv.sendmail(from_addr,[to_addr],msgrot.as_string())
+
+
     srv.quit()
 except Exception as e:
   print(e)
